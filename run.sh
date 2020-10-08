9fb7d476e86f4100348c5215876203954539f577
Edson Tadeu M. Manoel <tadeu@esss.co>
2020-10-08 09:24:05 -0300

    Rename diff files for better syntax coloring

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 pytest . --import-mode=importlib -s --no-header --no-summary >pytest-output.txt
 pytest a b --import-mode=importlib -s --no-header --no-summary >pytest-output-multiple-roots.txt
 python manual.py >manual-output.txt
-diff -u manual-output.txt pytest-output.txt >diff1.txt
-diff -u manual-output.txt pytest-output-multiple-roots.txt >diff2.txt
+diff -u manual-output.txt pytest-output.txt >diff1.diff
+diff -u manual-output.txt pytest-output-multiple-roots.txt >diff2.diff
